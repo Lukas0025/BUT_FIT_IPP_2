2aead2ce457de3ba1c71fb144df18ffc6208be92
Lukáš Plevač <lukasplevac@gmail.com>
2021-04-18 15:06:38 +0000

    Added headers to program_file.py

--- src/interpret/program_file.py
+++ src/interpret/program_file.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+##
+# Project: ipp2
+# file with ipp21 interpret implementation (TOP)
+# @author Lukáš Plevač <xpleva07>
+# @date 18.4.2021
+
 import xml.etree.ElementTree as ET
 import math
 import errors
